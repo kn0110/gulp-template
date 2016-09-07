1b9667542042d9848f28ca6deb6bd0f1ddaf97da
yama-dev <tatsuya.yamamoto69@gmail.com>
2016-09-08 00:28:38 +0900

    adjust gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,41 @@
-var gulp = require("gulp"),
-  browser = require("browser-sync"),
-  reload = browser.reload,
-  runSequence = require('run-sequence'),
-  del = require('del'),
-  plugins = require("gulp-load-plugins")();
+'use strict';
+/**
+ * 環境設定
+ */
+var CONFIG = {
+  outputDirectory: {
+    dev : './src/',
+    release : './release/',
+  },
+  sourceDirectory: {
+    sass : './src/**/*.scss',
+    js : './src/assets/js',
+  },
+  watchDirectory: {
+    html : './src/**/*.html',
+    css : './src/**/*.css',
+    sass : './src/**/*.scss',
+    js : './src/**/*.js',
+  }
+};
+var SASS_AUTOPREFIXER_BROWSERS = [
+  'ie >= 8',
+  'ios >= 7',
+  'android >= 2.3',
+  'last 4 versions'
+];
+var SASS_OUTPUT_STYLE = "expanded"; //nested, compact, compressed, expanded.
+
+/**
+ * IMPORT MODULES
+ */
+var gulp = require("gulp");
+var browser = require("browser-sync");
+var reload = browser.reload;
+var runSequence = require('run-sequence');
+var del = require('del');
+var plugins = require("gulp-load-plugins")();
+
 var DevDir = './src/',
   ReleaseDir = './release/',
   SassDir = './src/**/*.scss',
@@ -12,49 +44,65 @@ var DevDir = './src/',
   CssWatchdir = './src/**/*.css',
   SassWatchdir = './src/**/*.scss';
 
-//---default task---
+/**
+ * Sass Task
+ */
 gulp.task('sass', function() {
-  gulp.src(SassDir)
-    .pipe(plugins.plumber())
-    .pipe(plugins.sass({outputStyle: 'expanded'})) //nested, compact, compressed, expanded.
+  gulp.src(CONFIG.sourceDirectory.sass)
+    .pipe(plumber())
+    .pipe(sass({outputStyle: SASS_OUTPUT_STYLE}))
+    //ファイルの結合に使用
     //.pipe(concat('concat.css'))
-    .pipe(plugins.pleeease({
-      autoprefixer: {"browsers": ["last 4 versions",'ie 8', "Android 2.3"]},
+    .pipe(pleeease({
+      autoprefixer: {"browsers": SASS_AUTOPREFIXER_BROWSERS},
       minifier: false
     }))
-    //.pipe(header('@charset "utf-8";\n'))
     .pipe(gulp.dest(DevDir))
     .pipe(browser.reload({stream:true}));
 });
+
+/**
+ * HtmlLint Task
+ */
+gulp.task('htmllint', () => {
+  return gulp.src([CONFIG.watchDirectory.html])
+  .pipe(plumber())
+  .pipe(htmlhint('.htmlhintrc'))
+  .pipe(htmlhint.reporter())
+});
+
+/**
+ * Reload Task
+ */
 gulp.task("reload",function() {
   gulp.src().pipe(browser.reload({stream:true}));
 });
+
+/**
+ * Watch Task
+ */
 gulp.task("watch",['server'], function() {
-  gulp.watch(Es6Dir,["babel"]);
-  gulp.watch(TypeDir,["typescript"]);
-  gulp.watch(SassWatchdir,["sass"]);
+  gulp.watch(CONFIG.HtmlWatchdir,["typescript"]);
+  gulp.watch(CONFIG.SassWatchdir,["sass"]);
 });
+
+/**
+ * Server Task
+ */
 gulp.task("server", function() {
-/*
-  plugins.connect-php.server({
-    port: 8000,
-    base: DevDir
-  }, function (){
-    browser({
-    proxy: 'localhost:8000'
-  });
-*/
   browser({
     server: {
       baseDir: DevDir
     }
   });
-  gulp.watch(HtmlWatchdir, reload);
-  gulp.watch(PhpWatchdir, reload);
+  gulp.watch(CONFIG.HtmlWatchdir, reload);
 });
 
-//Default Task
+/**
+ * Default Task
+ */
 gulp.task('default', function(callback) {
-  return runSequence(['sass'],'watch',callback);
+  return runSequence('watch',callback);
+  // return runSequence(['sass','htmllint'],'watch',callback);
 });
 
